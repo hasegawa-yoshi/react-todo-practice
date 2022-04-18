abf1a7e3b6df24a70a8b097a7e912a3e5c0c4ee7
hasegawa-yoshi <101229468+hasegawa-yoshi@users.noreply.github.com>
2022-04-18 16:30:27 +0900

    App 追加機能実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
+
   const [incompleteTodos, setIncompleteTodos] = useState([
     "ああああ",
     "いいいい"
@@ -9,11 +11,24 @@ export const App = () => {
 
   const [completeTodos, setCompleteTodos] = useState(["うううう"]);
 
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="todoを入力" />
-        <button>追加</button>
+        <input
+          placeholder="todoを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了リスト</p>
